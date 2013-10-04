fc314bf7f14c18802d151559dba371d66f66d863
Doug Aaser <prayhopesmile@Doug-Aasers-MacBook.local>
2013-10-04 17:48:02 -0400

    Initial Commit

---
+++ src/Droidmote/src/com/example/droidmote/IconMetaData.java
@@ -0,0 +1,10 @@
+package com.example.droidmote;
+
+public class IconMetaData 
+{
+	private String AppName = null;
+	private int CellState = 0;
+	private String AppIconPath = null;
+	private String AppMainActivity = null;
+
+}
