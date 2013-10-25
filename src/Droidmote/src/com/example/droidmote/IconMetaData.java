fd15ef111370ca48dec78c0b19fe6a77c986c590
Doug Aaser <prayhopesmile@Doug-Aasers-MacBook.local>
2013-10-25 17:44:01 -0400

    2nd Update to Droidmote project

--- src/Droidmote/src/com/example/droidmote/IconMetaData.java
+++ src/Droidmote/src/com/example/droidmote/IconMetaData.java
@@ -7,28 +7,38 @@ public class IconMetaData
 	private String m_AppIconPath = null;
 	private String m_AppMainActivity = null;
 	private int m_CellIndex = 0;
+	private String m_PackageName = null;
+	private String m_ClassName = null;
 
 	/*
 	 * The following code is used to set the meta data.
 	 * */
 	public void setAppName(String appName) {
-		
+		m_AppName = appName;
 	}
 	
 	public void setCellState(int cellState) {
-		
+		m_CellState = cellState;
 	}
 	
 	public void setAppIconPath(String iconPath) {
-		
+		m_AppIconPath = iconPath;
 	}
 	
 	public void setAppMainActivity(String appMainActivity) {
-		
+		m_AppMainActivity = appMainActivity;
 	}
 	
 	public void setCellIndex(int index) {
-		
+		m_CellIndex = index;
+	}
+	
+	public void setPackageName(String packageName) {
+		m_PackageName = packageName;
+	}
+	
+	public void setClassName(String className) {
+		m_ClassName = className;
 	}
 	
 	/*
@@ -36,26 +46,34 @@ public class IconMetaData
 	 * */
 	public String getAppName() {
 		
-		return null;
+		return m_AppName;
 	}
 	
 	public int getCellState() {
 		
-		return 0;
+		return m_CellState;
 	}
 	
 	public String getAppIconPath() {
 		
-		return null;
+		return m_AppIconPath;
 	}
 	
 	public String getAppMainActivity() {
 		
-		return null;
+		return m_AppMainActivity;
 	}
 	
 	public int getCellIndex() {
 		
-		return 0;
+		return m_CellIndex;
+	}
+	
+	public String getPackageName() {
+		return m_PackageName;
+	}
+	
+	public String getClassName() {
+		return m_ClassName;
 	}
 }
